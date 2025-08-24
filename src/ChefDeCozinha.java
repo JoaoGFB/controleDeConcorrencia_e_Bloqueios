018a79dac9b8982dc02a6ae27e1c70a3fa32591d
JoaoPauloMoroVenancioDaSilva <joaopaulo.moro@edu.unifil.br>
2025-08-24 15:48:56 -0300

    Update ChefDeCozinha.java

--- src/ChefDeCozinha.java
+++ src/ChefDeCozinha.java
@@ -7,25 +7,29 @@ import java.util.List;
 public class ChefDeCozinha {
     private final List<Bloqueio> bloqueios = new ArrayList<>();
 
-    public synchronized boolean solicitarBloqueio(Garcom garcom, Pedido ingrediente, TipoBloqueio tipo) {
+    public boolean solicitarBloqueio(Garcom garcom, Pedido ingrediente, TipoBloqueio tipo) {
         Iterator<Bloqueio> it = bloqueios.iterator();
         while (it.hasNext()) {
             Bloqueio b = it.next();
             if (b.getRecurso().equals(ingrediente)) {
                 if (!b.eCompativel(tipo, garcom.getName())) {
-                    return false; // apenas retorna false sem imprimir
+                    return false;
                 }
             }
         }
-        Bloqueio novo = new Bloqueio(ingrediente, tipo, garcom.getName());
-        bloqueios.add(novo);
-        System.out.println("Bloqueio " + tipo + " concedido para " + garcom.getName() +
-                " em " + ingrediente.getNome());
+        bloqueios.add(new Bloqueio(ingrediente, tipo, garcom.getName()));
+        System.out.println("Bloqueio " + tipo + " concedido para " + garcom.getName() + " em " + ingrediente.getNome());
         return true;
     }
 
-    public synchronized void liberarBloqueios(Garcom garcom) {
+    public void liberarBloqueios(Garcom garcom) {
         bloqueios.removeIf(b -> b.getDono().equals(garcom.getName()));
         System.out.println("Chef liberou todos os bloqueios de " + garcom.getName());
+    }
+
+    // Resolve deadlock liberando apenas bloqueios que travam outros
+    public void resolverDeadlock() {
+        System.out.println(">>> CHEF liberando bloqueios para resolver deadlock...");
+        bloqueios.clear(); // simplificação: libera todos os bloqueios para continuar
     }
 }
